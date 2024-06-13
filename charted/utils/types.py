3cceabf35b368ccb35ac64e9be10c8af4d5e8333
Andryo Marzuki <stabbish@gmail.com>
2024-06-13 18:21:06 +1200

    remove unnecessary types

--- charted/utils/types.py
+++ charted/utils/types.py
@@ -7,19 +7,6 @@ Vector = list[float]
 Vector2D = list[Vector]
 
 
-class Bounds(NamedTuple):
-    x1: float
-    x2: float
-    y1: float
-    y2: float
-
-
-class RectDimensions(NamedTuple):
-    x_width: float
-    column_gap: float
-    rect_coordinates: float
-
-
 class MeasuredText(NamedTuple):
     text: str
     width: float
