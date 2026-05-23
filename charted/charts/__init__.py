2afa05135ff8b4c88ca8133ad2fe911f030e61ef
Andryo Marzuki <42439397+marzukia@users.noreply.github.com>
2026-05-23 14:10:41 +1000

    add heatmap chart type

--- charted/charts/__init__.py
+++ charted/charts/__init__.py
@@ -4,6 +4,7 @@ from .bar import BarChart
 from .chart import Chart
 from .column import ColumnChart
 from .gantt import GanttChart
+from .heatmap import HeatmapChart
 from .line import LineChart
 from .pie import PieChart
 from .radar import RadarChart
@@ -13,6 +14,7 @@ __all__ = [
     "BarChart",
     "ColumnChart",
     "GanttChart",
+    "HeatmapChart",
     "LineChart",
     "PieChart",
     "RadarChart",
